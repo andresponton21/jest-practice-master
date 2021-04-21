9e145614e1ba082514fd3d20df7bf69df0b84798
andres <andresponton21@yahoo.com>
2021-04-21 15:41:04 -0400

    finished with the cookbook test

--- src/cookbook.js
+++ src/cookbook.js
@@ -17,7 +17,28 @@ class Cookbook {
 
   removeRecipe(name) {
     delete this.recipes[name];
+    return `${name} deleted`
   }
 }
 
 module.exports = { Cookbook };
+
+// const myCookBook = new Cookbook
+// myCookBook.addRecipe('capuchino',['milk','cofee','cream'])
+// // myCookBook.addRecipe('black coffe',['water','cofee','sugar'])
+
+
+// console.log(myCookBook.recipes['capuchino'].join(', '))
+
+
+// console.log(myCookBook.getRecipe('capuchino'))
+// console.log(myCookBook.listRecipes())
+
+// // console.log(myCookBook.removeRecipe('capuchino'))
+// // console.log(myCookBook.listRecipes())
+// // const myCookBook = new Cookbook
+// // const recipeName = 'capuchino'
+// // const recipeIngs = ['milk','cofee','cream']
+// // myCookBook.addRecipe(recipeName, recipeIngs)  
+// // console.log(myCookBook.recipes)
+// // console.log(recipeName, recipeIngs)
--- test/cookbook.test.js
+++ test/cookbook.test.js
@@ -4,23 +4,55 @@ describe('Cookbook', () => {
   describe('Adding recipes', () => {
     test('should allow a recipe to be added', () => {
 
+      const myCookBook = new Cookbook
+      const recipeName = 'capuchino'
+      const recipeIngs = ['milk','cofee','cream']
+      myCookBook.addRecipe(recipeName, recipeIngs)  
+
+      expect(myCookBook.recipes).toEqual({ capuchino: [ 'milk', 'cofee', 'cream' ] })
+
     });
   });
 
   describe('Listing recipes', () => {
     test('should allow the recipes to be listed', () => {
+      const myCookBook = new Cookbook
+      const recipeName = 'capuchino'
+      const recipeIngs = ['milk','cofee','cream']
+      myCookBook.addRecipe(recipeName, recipeIngs)  
+      const myRecipes = myCookBook.listRecipes()
+
+
+      expect(myRecipes).toEqual([ 'capuchino' ])
 
     });
   });
 
   describe('Retrieving a recipe', () => {
     test('should allow the ingredients for a recipe to be retrieved', () => {
+      const myCookBook = new Cookbook
+      const recipeName = 'capuchino'
+      const recipeIngs = ['milk','cofee','cream']
+      myCookBook.addRecipe(recipeName, recipeIngs)  
+      const getMyRecipe = myCookBook.getRecipe(recipeName)
+
+
+      expect(getMyRecipe).toEqual(['milk','cofee','cream'])
 
     });
   });
 
   describe('Deleting a recipe', () => {
     test('should allow a recipe to be deleted', () => {
+
+      const myCookBook = new Cookbook
+      const recipeName = 'capuchino'
+      const recipeIngs = ['milk','cofee','cream']
+      myCookBook.addRecipe(recipeName, recipeIngs)  
+      const removeMyrecipe = myCookBook.removeRecipe(recipeName)
+
+
+      expect(myCookBook.recipes).toEqual({})
 
     });
   });
