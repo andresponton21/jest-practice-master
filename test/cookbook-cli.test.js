edb001c0480b3fae4a0a202ad87bf7de621c2633
andres <andresponton21@yahoo.com>
2021-04-21 17:06:57 -0400

    test cli

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -4,6 +4,14 @@ const { CookbookCli } = require('../src/cookbook-cli');
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
+      const myCookbook = new Cookbook
+      const myCookCli = new CookbookCli(myCookbook)
+      const message = myCookCli.run('add', 'cake', ['flour ', 'sugar', 'eggs'])
+      
+
+
+      expect(message).toEqual(`Successfully added the following recipe: cake`)
+
 
     });
   });
